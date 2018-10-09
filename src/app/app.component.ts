21ff566138e51e8bcee766cf2e66fb1c6eeb2033
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-10-09 12:22:47 +0200

    Customize product images outlet ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductDetailOutlets } from '@spartacus/storefront';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cxlive';
+  pdpOutlets = ProductDetailOutlets;
 }

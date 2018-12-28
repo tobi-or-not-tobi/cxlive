dd8358951fb73d4fe9c60e2d6513d9461aaa9288
Tobias Ouwejan <tobias.ouwejan@sap.com>
2018-12-28 16:27:59 +0100

    removing obsolete import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { StorefrontModule } from '@spartacus/storefront';
-import { CustomSearchModule } from './custom-search/custom-search.module';
 @NgModule({
   declarations: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],

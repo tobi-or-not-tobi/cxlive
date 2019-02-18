d6ef53f78b36274a7ba6c70d6a404f7b22581588
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-02-18 09:35:18 +0100

    teporary workaround for the missing export of SearchBoxComponentService

--- src/app/custom-search/custom-search.component.ts
+++ src/app/custom-search/custom-search.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { ɵbo as SearchBoxComponentService } from '@spartacus/storefront';
 @Component({
   selector: 'app-custom-search',
   templateUrl: './custom-search.component.html',
-  styleUrls: ['./custom-search.component.scss']
+  styleUrls: ['./custom-search.component.scss'],
+  providers: [SearchBoxComponentService]
 })
 export class CustomSearchComponent {
   query;
